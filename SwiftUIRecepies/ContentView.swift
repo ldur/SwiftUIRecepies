6d4bf00f62f08e8ef9c99433dc3edae98071c54e
ldur <lasse@durucz.com>
2023-10-24 12:20:19 +0200

    Initial Commit

---
+++ SwiftUIRecepies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIRecepies
+//
+//  Created by Lasse Durucz on 24/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIRecepies/SwiftUIRecepiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRecepiesApp.swift
+//  SwiftUIRecepies
+//
+//  Created by Lasse Durucz on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRecepiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
